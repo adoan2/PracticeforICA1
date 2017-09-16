c77cb98e8089cd1ff107c5b05ca9ac14c5046f1d
adoan2 <amandavdoan@gmail.com>
2017-09-16 16:04:09 -0600

    Made a practice1 for ICA

---
+++ Practice1_Website/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice1_Website.Startup))]
+namespace Practice1_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
